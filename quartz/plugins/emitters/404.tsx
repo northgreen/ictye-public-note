1dd5097680c7eca1e49481176f642b1db165c007
Ictye(jj) <northgreen2006@qq.com>
2026-02-05 00:20:53 +0800

    update

--- quartz/plugins/emitters/404.tsx
+++ quartz/plugins/emitters/404.tsx
@@ -5,71 +5,71 @@ import { pageResources, renderPage } from "../../components/renderPage"
 import { FullPageLayout } from "../../cfg"
 import { FullSlug } from "../../util/path"
 import { sharedPageComponents } from "../../../quartz.layout"
-import { NotFound,Flex,Darkmode,ReaderMode,Search,Explorer } from "../../components"
+import { NotFound, Flex, Darkmode, ReaderMode, Search, Explorer } from "../../components"
 import { defaultProcessedContent } from "../vfile"
 import { write } from "./helpers"
 import { i18n } from "../../i18n"
 
 export const NotFoundPage: QuartzEmitterPlugin = () => {
-  const opts: FullPageLayout = {
-    ...sharedPageComponents,
-    pageBody: NotFound(),
-    beforeBody: [],
-    left: [
-        Flex({
-            components: [
-                {
-                    Component: Search(),
-                    grow: true,
-                },
-                { Component: Darkmode() },
-                { Component: ReaderMode() },
-            ],
-        }),
-        Explorer(),
-    ],
-    right: [],
-  }
+    const opts: FullPageLayout = {
+        ...sharedPageComponents,
+        pageBody: NotFound(),
+        beforeBody: [Search()],
+        left: [
+            Flex({
+                components: [
+                    {
+                        Component: Search(),
+                        grow: true,
+                    },
+                    { Component: Darkmode() },
+                    { Component: ReaderMode() },
+                ],
+            }),
+            Explorer(),
+        ],
+        right: [],
+    }
 
-  const { head: Head, pageBody, footer: Footer } = opts
-  const Body = BodyConstructor()
+    const { head: Head, pageBody, footer: Footer } = opts
+    const Body = BodyConstructor()
 
-  return {
-    name: "404Page",
-    getQuartzComponents() {
-      return [Head, Body, pageBody, Footer]
-    },
-    async *emit(ctx, _content, resources) {
-      const cfg = ctx.cfg.configuration
-      const slug = "404" as FullSlug
+    return {
+        name: "404Page",
+        getQuartzComponents() {
+            return [Head, Body, pageBody, Footer]
+        },
+        async *emit(ctx, _content, resources) {
+            const cfg = ctx.cfg.configuration
+            const slug = "404" as FullSlug
 
-      const url = new URL(`https://${cfg.baseUrl ?? "localhost:8080"}`)
-      const path = url.pathname as FullSlug
-      const notFound = i18n(cfg.locale).pages.error.title
-      const [tree, vfile] = defaultProcessedContent({
-        slug,
-        text: notFound,
-        description: notFound,
-        frontmatter: { title: notFound, tags: [] },
-      })
-      const externalResources = pageResources(path, resources)
-      const componentData: QuartzComponentProps = {
-        ctx,
-        fileData: vfile.data,
-        externalResources,
-        cfg,
-        children: [],
-        tree,
-        allFiles: [],
-      }
+            const url = new URL(`https://${cfg.baseUrl ?? "localhost:8080"}`)
+            const path = url.pathname as FullSlug
+            const notFound = i18n(cfg.locale).pages.error.title
+            const [tree, vfile] = defaultProcessedContent({
+                slug,
+                text: notFound,
+                description: notFound,
+                frontmatter: { title: notFound, tags: [] },
+            })
+            const externalResources = pageResources(path, resources)
+            const componentData: QuartzComponentProps = {
+                ctx,
+                fileData: vfile.data,
+                externalResources,
+                cfg,
+                children: [],
+                tree,
+                allFiles: [],
+            }
 
-      yield write({
-        ctx,
-        content: renderPage(cfg, slug, componentData, opts, externalResources),
-        slug,
-        ext: ".html",
-      })
-    },
-    async *partialEmit() {},
-  }
+            yield write({
+                ctx,
+                content: renderPage(cfg, slug, componentData, opts, externalResources),
+                slug,
+                ext: ".html",
+            })
+        },
+        async *partialEmit() { },
+    }
 }
